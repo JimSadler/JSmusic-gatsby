62a87622e0feac76b313cce042920630a638ea8e
JimSadler <jim@jsdevelopment.tech>
2020-07-01 08:56:33 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,20 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-prismic-graphql',
+      options: {
+        repositoryName: 'jsmusic1',
+        pages: [{
+          type: 'Page',
+          match: '/:uid',
+          path: '/',
+          component: require.resolve('./src/templates/page.js')
+        }]
+      }
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ src/templates/page.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { RichText } from 'prismic-reactjs';
+import Layout from '../components/layout'
+
+// const HeadImg = styled.img`
+//   border-radius: 50%;
+//   width: 35%;
+//   border: .1rem solid #66FEFF;
+//   text-align: center;
+// `
+// const HeadImgContainer = styled.div`
+//   text-align: center;
+// `;
+export const query = graphql`
+query PageQuery($id: String) {
+  prismic {
+    allPages(id: $id) {
+      edges {
+        node {
+          page_title
+          header_img
+          content
+          _meta {
+            uid
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const Page = (props) => {
+  const pageTitle = props.data.prismic.allPages.edges[0].node.pagetitle;
+  const pageContent = props.data.prismic.allPages.edges[0].node.content;
+  const pageHeaderImg = props.data.prismic.allPages.edges[0].node.header_img.url;
+  return (
+    <Layout>
+      <RichText render={pageTitle} />
+      <div>
+        <img alt={props.data.prismic.allPages.edges[0].node.header_img.alt} src={pageHeaderImg} />
+      </div>
+
+      <RichText render={pageContent} />
+
+    </Layout>
+  )
+}
+export default Page;
